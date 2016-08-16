292f710ff7bf4e3ab2b908f09463537dae02af37
Sergei Zyubin <s.zyubin@infotech.team>
2016-08-16 18:09:48 +0300

    Add mocking test functions

--- tests/test_inflo.py
+++ tests/test_inflo.py
@@ -24,3 +24,37 @@ class TestInflo(unittest.TestCase):
         inflo.parser.invoke_get_tenant_info(args)
         inflo.get_info.assert_called_with(api_key=api_key, customer_id=customer_id, raw=False,
                                           url='https://api.flops.ru/api/v1/tenant/', table_format=['id', 'description'])
+
+    def test_inflo_invoke_server_list(self):
+        inflo.get_info = mock.MagicMock(return_value=0)
+        inflo.parser.invoke_server_list(args)
+        inflo.get_info.assert_called_with(api_key=args.api_key, customer_id=args.customer_id, raw=False,
+                                          url='https://api.flops.ru/api/v1/vm/', table_format=['id', 'name', 'memory',
+                                               'disk', 'cpu', 'ipAddresses', ('2', 'distribution', 'name')])
+
+    def test_inflo_invoke_os_list(self):
+        inflo.get_info = mock.MagicMock(return_value=0)
+        inflo.parser.invoke_os_list(args)
+        inflo.get_info.assert_called_with(api_key=args.api_key, customer_id=args.customer_id, raw=False,
+                                          url='https://api.flops.ru/api/v1/distribution/', table_format=['id', 'name',
+                                              'description', 'bitness'])
+
+    def test_inflo_invoke_get_software(self):
+        inflo.get_info = mock.MagicMock(return_value=0)
+        inflo.parser.invoke_get_software(args)
+        inflo.get_info.assert_called_with(api_key=args.api_key, customer_id=args.customer_id, raw=False,
+                                          url='https://api.flops.ru/api/v1/software/', table_format=['id', 'name'])
+
+    def test_inflo_invoke_get_tariffs(self):
+        inflo.get_info = mock.MagicMock(return_value=0)
+        inflo.parser.invoke_get_tariffs(args)
+        inflo.get_info.assert_called_with(api_key=args.api_key, customer_id=args.customer_id, raw=False,
+                                          url='https://api.flops.ru/api/v1/tariffs/', table_format=['id', 'name',
+                                          'memory', 'disk', 'cpu', 'ipCount', 'onDemand', 'forWindows'])
+
+    def test_inflo_invoke_get_pubkeys(self):
+        inflo.get_info = mock.MagicMock(return_value=0)
+        inflo.parser.invoke_get_pubkeys(args)
+        inflo.get_info.assert_called_with(api_key=args.api_key, customer_id=args.customer_id, raw=False,
+                        url='https://api.flops.ru/api/v1/pubkeys/', table_format=['id', 'name', 'type', 'publicKey',
+                             'timeAdded'])
